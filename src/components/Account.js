6e0c405f5c448101a1efc809beb100cc0166494b
Tristen Everett <spock214@gmail.com>
2022-11-18 16:45:44 -0800

    add sign up modal and styling

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,57 +1,131 @@
 import React from "react";
+import CustomModal from "./CustomModal";
 
 class Account extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      user: false,
+      authenticated: false,
       newUserModal: false
     };
   }
 
   //Styles Object
   styles = {
-
+    grid: {
+      display: "grid",
+      gridTemplateColumns: "15% auto 5% auto 35%",
+      gridTemplateRows: "10vh 10vh 10vh"
+    },
+    alignRight: {
+      display: "flex",
+      justifyContent: "end"
+    },
+    textBox: {
+      height: "3em",
+      width: "80%"
+    },
+    button: {
+      height: "3em",
+      width: "fit-content"
+    },
+    padding: {
+      paddingLeft: "5%",
+      paddingRight: "5%"
+    }
   };
+
+  //Modal Functions
+  modalShow = () => {
+    this.setState({ newUserModal: true });
+  }
+  modalHide = () => {
+    this.setState({ newUserModal: false });
+  }
 
   //Auth Functions
   tempSubmit = (event) => {
     event.preventDefault();
-    console.log("button clicked in Account component");
+    console.log("Submit button clicked in Account component");
     this.setState(prevState => ({
-      user: !prevState.user,
+      authenticated: !prevState.authenticated,
       newUserModal: false
     }));
   };
 
   //Return Logic
   render(){
-    if(this.state.user === true){
+    //Style Functions
+    function tablePosition(fromTop, fromLeft){
+      const resultString = fromTop + " / " + fromLeft;
+      return({ gridArea: resultString });
+    }
+    function stylesCombine(...args){
+      let result = {};
+      args.forEach((style) => {
+        result = {...result, ...style};
+      });
+      return result;
+    }
+
+    if(this.state.authenticated === true){
       return(
         <React.Fragment>
-          <h1>Sign out</h1>
-          <button onClick={this.tempSubmit}>Sign Out</button>
+          <h1 className="center">Sign out</h1>
+          <div className="center">
+            <button onClick={this.tempSubmit} style={this.styles.button}>Sign Out</button>
+          </div>
         </React.Fragment>
       );
     }
-    if(this.state.user === false){
+    if(this.state.authenticated === false){
       return(
         <React.Fragment>
-          {/* Modal load based on this.state.newUserModal to register account */}
-          <h1>Sign in</h1>
-          <form onSubmit={this.tempSubmit}>
-            <p>Email: </p>
-            <input
-              type="text"
-              name="signInEmail"
-              placeholder="name@example.com" />
-            <p>Password: </p>
-            <input
-              type="password"
-              name="signInPassword"
-              placeholder="PASSWORD" />
-            <button type="submit">Sign In</button>
-          </form>
+          {/* Modal will load when "Create Account" button is clicked */}
+          <CustomModal show={this.state.newUserModal} handleClose={this.modalHide}>
+            <h1 className="center">Sign Up</h1>
+            <form onSubmit={this.tempSubmit}>
+              <div style={this.styles.grid}>
+                <p style={stylesCombine(tablePosition(1, 2), this.styles.alignRight)}>Email: </p>
+                <input
+                  type="text"
+                  name="signUpEmail"
+                  placeholder="name@example.com"
+                  style={stylesCombine(tablePosition(1, 4), this.styles.textBox)} />
+                <p style={stylesCombine(tablePosition(2, 2), this.styles.alignRight)}>Password: </p>
+                <input
+                  type="password"
+                  name="signUpPassword"
+                  placeholder="PASSWORD"
+                  style={stylesCombine(tablePosition(2, 4), this.styles.textBox)} />
+                <button type="submit" style={stylesCombine(tablePosition(3, 5), this.styles.button)}>Sign Up</button>
+              </div>
+            </form>
+          </CustomModal>
+          
+          {/* Main page information to be loaded at all times */}
+          <div style={this.styles.padding}>
+            <h1>Sign in</h1>
+            <form onSubmit={this.tempSubmit}>
+              <div style={this.styles.grid}>
+                <p style={stylesCombine(tablePosition(1, 2), this.styles.alignRight)}>Email: </p>
+                <input
+                  type="text"
+                  name="signInEmail"
+                  placeholder="name@example.com"
+                  style={stylesCombine(tablePosition(1, 4), this.styles.textBox)} />
+                <p style={stylesCombine(tablePosition(2, 2), this.styles.alignRight)}>Password: </p>
+                <input
+                  type="password"
+                  name="signInPassword"
+                  placeholder="PASSWORD"
+                  style={stylesCombine(tablePosition(2, 4), this.styles.textBox)} />
+                <button type="submit" style={stylesCombine(tablePosition(3, 5), this.styles.button)}>Sign In</button>
+              </div>
+            </form>
+            <p>New User?</p>
+            <button onClick={this.modalShow} style={this.styles.button}>Create Account</button>
+          </div>
         </React.Fragment>
       );
     }

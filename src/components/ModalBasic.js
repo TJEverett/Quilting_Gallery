6fd215ebbea852acdf1196135485bb9075d64205
Tristen Everett <spock214@gmail.com>
2023-07-07 13:59:03 -0700

    delete CustomModal and redirect calls to ModalBasic
     * Create and delete a display component for use in testing ModalBasic
       while comparing to CustomModal
    
     * Create TestModal and later rename to ModalBasic
    
     * Delete CustomModal and replace all calls in other components with
       calls to ModalBasic
    
     * Added global style for image tags that will resize down to fit based
       on max width and height of parent container
    
     * Added an image

---
+++ src/components/ModalBasic.js
@@ -0,0 +1,79 @@
+import React from "react";
+import PropTypes from "prop-types";
+import closeImage from "../images/mcol_cross.svg";
+
+function ModalBasic(props) {
+  //Style Object
+  const styles = {
+    base: {
+      position: "fixed",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%",
+      background: "rgba(0, 0, 0, 0.6)"
+    },
+    internal: {
+      position: "fixed",
+      background: "white",
+      width: "80vw",
+      height: "80vh",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)"
+    },
+    exit: {
+      display: "flex",
+      position: "fixed",
+      zIndex: "2",
+      width: "100%",
+      height: "2.25rem",
+      justifyContent: "right"
+    },
+    border: {
+      position: "relative",
+      zIndex: "1",
+      padding: "2.5rem",
+      background: "indigo",
+      color: "khaki",
+      height: "calc(100% - 5rem)",
+      width: "calc(100% - 5rem)"
+    }
+  }
+
+  //Style Logic
+  const modalOuterStyle = props.show ? { ...styles.base, display: "block" } : { ...styles.base, display: "none" };
+
+  return(
+    <div style={modalOuterStyle} onClick={() => {props.handleClose()}}>
+      <div style={styles.internal} onClick={(event) => {event.stopPropagation()}}>
+        <div style={styles.exit}>
+          <img src={closeImage} alt="Close" title="Close" onClick={() => props.handleClose()} />
+        </div>
+        <div className="center" style={styles.border}>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+ModalBasic.defaultProps = {
+  handleClose: () => {console.log("no function supplied to ModalBasic")},
+  show: false
+};
+
+ModalBasic.propTypes = {
+  handleClose: PropTypes.func,
+  show: PropTypes.bool,
+  children: PropTypes.any
+};
+
+export default ModalBasic;
+
+//NOTES FOR USE:
+// - modal is fixed at 80% X 80% of screen size
+// - modal has a 2.5rem padding around all sides
+// - contents passed to modal are not resized by modal before being displayed
+// - contents loaded inside a <div> with defined {height, width}
+// - clicking outside of modal will close the same as clicking the closing <image>

8e18ba81fa72730443c3244b0c497024d3167476
Tristen Everett <spock214@gmail.com>
2023-12-22 16:12:16 -0800

    add static temp data and update routing

--- src/components/SupplyRouter.js
+++ src/components/SupplyRouter.js
@@ -1,9 +1,54 @@
 import React from "react";
 import { useRouteMatch, Switch, Route, Redirect } from "react-router-dom";
 import SupplyCreateForm from "./SupplyCreateForm";
+import SupplyAllView from "./SupplyAllView";
 
 function SupplyRouter() {
   let { path, url } = useRouteMatch();
+  const supplyList = [
+    {
+      Name: "Purple Thread",
+      Location: "Box 1",
+      Description: "variegated",
+      Comments: "bought from Sharon",
+      ImageArray: [
+        "https://picsum.photos/1200/900",
+        "https://picsum.photos/900/1200"
+      ],
+      Stock: [
+        {amount: 3, unit: "units"}
+      ]
+    },
+    {
+      Name: "Princess Fabric",
+      Location: "Box 3",
+      Description: "Belle, Cinderella, Aurora, Elsa, and Rapunzel",
+      Comments: "bought @ the J-store",
+      ImageArray: [
+        "https://picsum.photos/1200/900",
+        "https://picsum.photos/900/1200"
+      ],
+      Stock: [
+        {amount: 2.33, unit: "yard"}
+      ]
+    },
+    {
+      Name: "Turtle Fabric",
+      Location: "Box 2",
+      Description: "colors are: blue-green, brown-green, and orange-brown",
+      Comments: "bought on trip to canada",
+      ImageArray: [
+        "https://picsum.photos/900/1200",
+        "https://picsum.photos/900/1200",
+        "https://picsum.photos/900/1200"
+      ],
+      Stock: [
+        {amount: 2, unit: "yard"},
+        {amount: 2, unit: "yard"},
+        {amount: 3, unit: "yard"}
+      ]
+    }
+  ];
 
   return (
     <Switch>
@@ -11,7 +56,7 @@ function SupplyRouter() {
         <Redirect from={url} to={`${url}/all`} />
       </Route>
       <Route path={`${url}/all`} >
-        <h2>All Supplies Route</h2>
+        <SupplyAllView supplyArray={supplyList}/>
       </Route>
       <Route path={`${url}/new`} >
         <SupplyCreateForm />

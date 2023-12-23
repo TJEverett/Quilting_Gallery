78be0a448ef65d4c81d0efef348fc04042191956
Tristen Everett <spock214@gmail.com>
2023-12-22 16:09:25 -0800

    create component to load image from URL prop with backup image

---
+++ src/components/Image.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import imageFail from "../images/mcol_cross.svg";
+
+class Image extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      src: props.src,
+      errored: false
+    };
+  }
+
+  onError = () => {
+    if(!this.state.errored){
+      this.setState({
+        src: imageFail,
+        errored: true
+      });
+    }
+  }
+
+  render(){
+    return(
+      <img src={this.state.src || imageFail} onError={this.onError} alt={this.props.alt} title={this.props.title} className={this.props.classes} style={this.props.styles} />
+    )
+  }
+}
+
+Image.defaultProps = {
+  src: "",
+  alt: "",
+  title: "",
+  classes: "",
+  styles: {}
+};
+
+Image.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  title: PropTypes.string,
+  classes: PropTypes.string,
+  styles: PropTypes.object
+};
+
+export default Image
